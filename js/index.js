ee21ed14ecf4d5fa39ecc3a32617bcae534c5886
Aditya Niloi <aditya@incl.us>
2018-04-16 12:35:03 -0400

    subtle overhaul

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Dyanmic page flow will go here
+$(document).ready(function() {
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: false,
+        duration: 150,
+        // noWrap: true
+    });
+});
+
+$('.right').click(function() {
+    $('.carousel').carousel('next');
+});
+
+$('.left').click(function() {
+    $('.carousel').carousel('prev');
+});
